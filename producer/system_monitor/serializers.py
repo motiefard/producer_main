d131b2353d46d691fc0c8367b86345bcbadbd93a
motiefard <mozhdemotieifard@gmail.com>
2025-12-27 14:05:50 +0330

    create model, serializer

---
+++ producer/system_monitor/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import SystemStatus
+
+
+class SystemStatusSerializer(serializers.ModelSerializer):
+    
+    ram_total_gb = serializers.ReadOnlyField()
+    disk_total_gb = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = SystemStatus
+        fields = [
+            'id',
+            'timestamp',
+            'created_at',
+            'ram_total',
+            'ram_used',
+            'ram_free',
+            'cpu_percent',
+            'disk_total',
+            'disk_used',
+            'disk_free',
+            'raw_data',
+        ]
+        read_only_fields = ['id', 'created_at']
+
+
