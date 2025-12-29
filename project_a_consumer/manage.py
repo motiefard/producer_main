a74263f2ac5766113e128d03f24b7d0c04fd4271
motiefard <mozhdemotieifard@gmail.com>
2025-12-29 13:28:42 +0330

    update a few places after renaming those folders

--- project_a_consumer/manage.py
+++ project_a_consumer/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'producer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consumer.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
